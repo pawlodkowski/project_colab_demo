65c572a25139e156b67a3836a2580d74676d86ba
Benedikt Hofbauer <ranis@online.de>
2021-06-07 14:34:54 +0200

    visualization.py should suck a little less now

--- visualization.py
+++ visualization.py
@@ -7,14 +7,18 @@ if __name__ == "__main__":
 
     background = np.zeros((700, 1000, 3), np.uint8)
 
+    print('omgomg the script is starting now enjoooooy!!!')
+
     while True:
         frame = background.copy()
-        rand_x = int(np.random.uniform(low=100, high=600))
-        rand_y = int(np.random.uniform(low=100, high=900))
+        rand_x = int(np.random.uniform(low=100, high=500))
+        rand_y = int(np.random.uniform(low=100, high=700))
         frame[rand_x : rand_x + 10, rand_y : rand_y + 10] = [0, 0, 255]
 
         cv2.imshow("frame", frame)
 
         key = chr(cv2.waitKey(1) & 0xFF)
         if key == "q":
+            print('I break now, goodbye')
+
             break
