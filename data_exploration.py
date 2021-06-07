ab7c018c08a625157d933b55dc5c7de626fd4d5b
Paul Wlodkowski <pawlodkowski@gmail.com>
2021-06-07 14:49:45 +0200

    improved explore function

--- data_exploration.py
+++ data_exploration.py
@@ -1,11 +1,13 @@
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('data/monday.csv')
+df = pd.read_csv("data/monday.csv")
+
 
 def explore():
-    pass 
+    """Nice function docstring"""
+    return None
 
-if __name__ == '__main__':
-    print('wow. such data. much exploration.')
 
+if __name__ == "__main__":
+    print("wow. such data. much exploration.")
